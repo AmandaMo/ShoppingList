0d2c5547716778c527d65faffe9260990fb076b2
AmandaMo <amanda.j.morale@gmail.com>
2015-03-25 18:40:34 -0500

    made responsive

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
 	
 	$('.add-item').on('click', function(e){
 		e.preventDefault();
+		if ($('.inputitem').val().length === 0)
+		{
+		}
+		else {
+		var newitem = $('.inputitem').val();
+		console.log(newitem);
+		$('.shopping-list').append(
+			"<li>"+
+				"<input type='checkbox' name='check1' class='column'>"+
+				"<p class='column'>"+
+				newitem +
+				"</p>"+
+				"<input type='image' src='images/xicon.svg' class='column delete-item' height='40em'/></li>"+
+			"</li>"
+		);
+		$('.inputitem').val('');
+		}
+	});
+
+	$('.add-item').keyup(function(e){
+		e.preventDefault();
 		var newitem = $('.inputitem').val();
 		console.log(newitem);
 		$('.shopping-list').append(
@@ -16,16 +37,19 @@ $(document).ready(function() {
 		$('.inputitem').val('');
 	});
 
+	$(document).on("click", ".checkbox", function(){
+		if ($('input.checkbox').is(':checked')) {
+	        $(this).next("p").toggleClass( 'checked');
+	        console.log("checked");
+	    }
+	    else{
 
-	$( '.checkboxes' ).on( 'click', 'input[type="checkbox"]', function () {
-    	$( this ).closest('p').toggleClass( '.checked');
+	    }
 	});
-
 
 	$(document).on("click", ".delete-item", function(){
-    	$(this).parent().remove();
+    	$(this).parent().fadeOut(600);
 	});
 	
-});	
-
+});
 
